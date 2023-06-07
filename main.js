8c86df28d0d36205d75ef397154f03b39af9684c
Rebroboga <Rebroboga10@mail.ru>
2023-06-07 20:10:36 +0300

    Portfolio
    Add project

---
+++ main.js
@@ -0,0 +1,8 @@
+// accordion
+
+new Accordion('.accordion__list', {
+  elementClass: 'accordion',
+  triggerClass: 'accordion__control',
+  panelClass: 'accordion__content',
+  activeClass: 'accordion--active'
+});
