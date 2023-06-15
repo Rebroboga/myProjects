37ad9d01e12a36e6fa815e6c7f7d2cb178188fcd
Rebroboga <Rebroboga10@mail.ru>
2023-06-15 17:12:48 +0300

    Add media adaptive
    add adaptive (mobile)

--- main.js
+++ main.js
@@ -55,6 +55,34 @@ fixed.forEach(item => {
 });
 
 
+// burger munu
+
+let burger = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__nav');
+let menuLinks = document.querySelectorAll('.header__link');
+
+burger.addEventListener('click', function() {
+
+  burger.classList.toggle('header__burger--active');
+
+  menu.classList.toggle('header__nav--active');
+
+  document.body.classList.toggle('stop-scroll')
+
+});
+
+menuLinks.forEach(function(element) {
+  element.addEventListener('click', function() {
+
+    burger.classList.remove('header__burger--active');
+
+    menu.classList.remove('header__nav--active')
+
+    document.body.classList.remove('stop-scroll')
+
+  })
+})
+
 
 
 
